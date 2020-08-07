0c24e2e82440916bfbc835e4b8ce2ab419bbff04
AmericaCube <cesare.rossi@gmail.com>
2020-08-07 16:47:12 +0200

    Update vmcegress.sh

--- vmcegress.sh
+++ vmcegress.sh
@@ -15,7 +15,7 @@
 
 clear
 
-echo "Update O.S. & Packages"
+echo "Updating O.S. & Packages"
 sudo yum update -y >/dev/null 2>&1
 
 echo "Installing Apache Web Server"
@@ -28,26 +28,26 @@ sudo firewall-cmd --permanent --add-port=80/tcp >/dev/null 2>&1
 sudo firewall-cmd --permanent --add-port=443/tcp >/dev/null 2>&1
 sudo firewall-cmd --reload >/dev/null 2>&1
 
-echo "Install wget"
+echo "Installing wget"
 sudo yum install -y wget >/dev/null 2>&1
 
-echo "Install Microsoft Powershell for Linux"
+echo "Installing Microsoft Powershell for Linux"
 curl -s https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo >/dev/null 2>&1
 sudo yum install -y powershell >/dev/null 2>&1
 
-echo "Update O.S. & Packages"
+echo "Updating O.S. & Packages"
 sudo yum update -y >/dev/null 2>&1
 
-echo "Install VMware Powershell Modules"
+echo "Installing VMware Powershell Modules"
 pwsh -command Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted" <<< A  >/dev/null 2>&1
 pwsh -command Install-Module "VMware.PowerCLI" -Scope "AllUsers" -AllowClobber -Force >/dev/null 2>&1
 pwsh -command Set-PowerCLIConfiguration -InvalidCertificateAction Ignore <<< A >/dev/null 2>&1
 pwsh -command Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP \$true <<< A >/dev/null 2>&1
 
-echo "Install William Lam NSX-T Powershell Module"
+echo "Installing William Lam NSX-T Powershell Module"
 pwsh -command Install-Module VMware.VMC.NSXT <<< A >/dev/null 2>&1
 
-echo "Download VMC-EGRESS Sctips from GitHub"
+echo "Downloading VMC-EGRESS Sctips from GitHub"
 wget --quiet https://github.com/AmericaCube/VMC-EGRESS/raw/master/vmc.ini
 wget --quiet https://github.com/AmericaCube/VMC-EGRESS/raw/master/vmc.ps1
 
